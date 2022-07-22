953f68ffe95bdce5162d34c9238c6f755aba78ba
HaJunYoo <hajuny129@gmail.com>
2022-07-23 00:15:27 +0900

    lec10

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -133,6 +133,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# staticfiles 라는 root에 모든 static 파일을 모으겠다.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
