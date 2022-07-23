152a10dc14f40966edfbab9b9df5dfa6f5c5b51d
HaJunYoo <hajuny129@gmail.com>
2022-07-24 02:01:10 +0900

    Bug Fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -47,11 +47,13 @@ class AccountDetailView(DetailView):
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world') # hello world url로 redirect
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login') # login url로 redirect
     template_name = 'accountapp/delete.html'
